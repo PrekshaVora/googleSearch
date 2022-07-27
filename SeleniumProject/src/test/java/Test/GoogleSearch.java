49719c14fc9df9a1132f58ffbde3a2609b964abd
PrekshaVora <83230546+PrekshaVora@users.noreply.github.com>
2022-07-27 23:22:03 +0530

    Add files via upload

---
+++ SeleniumProject/src/test/java/Test/GoogleSearch.java
@@ -0,0 +1,41 @@
+package Test;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+
+
+
+public class GoogleSearch {
+
+	public static WebDriver driver;
+	public static void main(String[] args) {
+	
+		String path = System.getProperty("user.dir")+ "\\src\\test\\resources\\Driver\\chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver",path);
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+		
+		//identify element
+		WebElement searchTextfield = driver.findElement(By.name("q"));
+		searchTextfield.sendKeys("Selenium java"); //enter text with sentKey()
+		
+		//Explicit wait
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//ul")));
+		searchTextfield.submit();
+		driver.close();
+//		
+	}
+
+	
+}
