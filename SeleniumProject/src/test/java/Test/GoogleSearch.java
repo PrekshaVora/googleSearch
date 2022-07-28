543ff28731eed0b190f0c85d5d590265148f9bea
PrekshaVora <vora.preksha2001@gmail.com>
2022-07-28 16:56:02 +0530

    add yamlfilehandler
    added yamlfilehandler code and the following codes;
    1. create drivers package & add chromedriver in it
    2. change path of driver to dynamically pick driver for above package
    3. add  testng assertion
    4. add implicit wait
    5. push this code to repo googleSearch
    6. create Yaml file and put application url
    7. create YamlFileHandler class and add function to read that file
    8. read application url in the GoogleSerach class from yaml file
    9. remove thread.sleep

--- SeleniumProject/src/test/java/Test/GoogleSearch.java
+++ SeleniumProject/src/test/java/Test/GoogleSearch.java
@@ -1,14 +1,18 @@
 package Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.xmlbeans.impl.xb.xsdschema.ListDocument.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
@@ -16,26 +20,33 @@ import org.testng.annotations.Test;
 
 public class GoogleSearch {
 
-	public static WebDriver driver;
-	public static void main(String[] args) {
-	
+	@Test
+	public void google_Serach() throws FileNotFoundException {
+		
+		WebDriver driver;
+		String url;
+		YamlFileHandler obj = new YamlFileHandler();
+		url=obj.readData();
+		
 		String path = System.getProperty("user.dir")+ "\\src\\test\\resources\\Driver\\chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver",path);
-		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.google.com");
+		driver = new ChromeDriver();
+		driver.get(url);
+		Assert.assertEquals("Google",driver.getTitle());
 		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
 		
 		//identify element
 		WebElement searchTextfield = driver.findElement(By.name("q"));
 		searchTextfield.sendKeys("Selenium java"); //enter text with sentKey()
 		
+		
 		//Explicit wait
 		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//ul")));
 		searchTextfield.submit();
-		driver.close();
-//		
+		driver.close();		
 	}
 
-	
 }
+
+	
